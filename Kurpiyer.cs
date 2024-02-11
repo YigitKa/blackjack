cbc26581db56575ee57505d0a7332eb9d718b1d7
Yiğit KATIRCIOĞLU <90yigit@gmail.com>
2024-02-11 14:32:08 +0300

    kurpiyer sınıfına eventler eklenmeye başlandı

--- Kurpiyer.cs
+++ Kurpiyer.cs
@@ -1,54 +1,80 @@
-﻿using System;
-using static Blackjack.Blackjack;
+﻿using Blackjack;
 
-namespace Blackjack
+public class KurpiyerKartCekildiHandler
 {
-    public class Kurpiyer
+    public void OnKurpiyerKartCekildi(object sender, EventArgs e)
     {
-        public List<Kart> Kartlari { get; set; }
-        public int Puan { get; set; }
-
-        public Kurpiyer()
-        {
-            Kartlari = new List<Kart>();
-        }
-
-        public void KartCek(List<Kart> deste)
-        {
-            Kart kart = Kart.RastgeleCek(deste);
-            Kartlari.Add(kart);
-            Puan = PuanHesapla();
-        }
-
-        public int PuanHesapla()
-        {
-            int puan = 0;
-            bool asVar = false;
-
-            foreach (Kart kart in Kartlari)
-            {
-                if (kart.Deger == KartDegeri.As)
-                {
-                    asVar = true;
-                    puan += 1;
-                }
-                else if ((int)kart.Deger <= 10)
-                {
-                    puan += (int)kart.Deger;
-                }
-                else
-                {
-                    puan += 10;
-                }
-            }
-
-            if (asVar && puan <= 11)
-            {
-                puan += 10;
-            }
-
-            return puan;
-        }
+        // Kurpiyer kart çekme işlemini gerçekleştirir.
     }
 }
 
+public class KurpiyerPuanHesaplandiHandler
+{
+    public void OnKurpiyerPuanHesaplandi(object sender, EventArgs e)
+    {
+        // Kurpiyerin puanını hesaplar.
+    }
+}
+
+public delegate void KurpiyerKartCekildiEventHandler(object sender, EventArgs e);
+public delegate void KurpiyerPuanHesaplandiEventHandler(object sender, EventArgs e);
+
+public class Kurpiyer
+{
+    public event KurpiyerKartCekildiEventHandler KurpiyerKartCekildi;
+    public event KurpiyerPuanHesaplandiEventHandler KurpiyerPuanHesaplandi;
+
+    private List<Kart> _kartlar;
+    private int _puan;
+
+    public List<Kart> Kartlari => _kartlar;
+
+    public int Puan => _puan;
+
+    public Kurpiyer()
+    {
+        _kartlar = new List<Kart>();
+    }
+
+    public void KartCek(List<Kart> deste)
+    {
+        Kart kart = Kart.RastgeleCek(deste);
+        _kartlar.Add(kart);
+
+        KurpiyerKartCekildi?.Invoke(this, EventArgs.Empty);
+
+        _puan = PuanHesapla();
+        KurpiyerPuanHesaplandi?.Invoke(this, EventArgs.Empty);
+    }
+
+    public int PuanHesapla()
+    {
+        int puan = 0;
+        bool asVar = false;
+
+        foreach (Kart kart in _kartlar)
+        {
+            if (kart.Deger == KartDegeri.As)
+            {
+                asVar = true;
+                puan += 1;
+            }
+            else if ((int)kart.Deger <= 10)
+            {
+                puan += (int)kart.Deger;
+            }
+            else
+            {
+                puan += 10;
+            }
+        }
+
+        if (asVar && puan <= 11)
+        {
+            puan += 10;
+        }
+
+        return puan;
+    }
+
+}
